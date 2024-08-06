3885b597d49badd380848034c561cc173af2f6b1
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-06 13:34:53 -0300

    generating components

---
+++ src/app/acesso/acesso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-acesso',
+  standalone: true,
+  imports: [],
+  templateUrl: './acesso.component.html',
+  styleUrl: './acesso.component.css'
+})
+export class AcessoComponent {
+
+}
