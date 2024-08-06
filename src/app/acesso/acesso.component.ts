ff2f1a3355bdd8b2cb24df69f2489c38f6323e9b
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-06 13:58:13 -0300

    updating components' css and html files

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from "./banner/banner.component";
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-acesso',
   standalone: true,
-  imports: [],
+  imports: [BannerComponent, LoginComponent],
   templateUrl: './acesso.component.html',
   styleUrl: './acesso.component.css'
 })
