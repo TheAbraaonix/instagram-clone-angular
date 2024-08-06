2435290abb0242e5f0fb51d6d0beb20bcb62d9de
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-06 15:34:35 -0300

    creating animations - part 1

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { LoginComponent } from "./login/login.component";
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   standalone: true,
   imports: [BannerComponent, LoginComponent],
   templateUrl: './acesso.component.html',
-  styleUrl: './acesso.component.css'
+  styleUrl: './acesso.component.css',
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(-50px, 0px)' }),
+        animate('500ms 0s ease-in-out')])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0px)' }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent {
-
+  public estadoBanner: string = 'criado';
+  public estadoPainel: string = 'criado';
 }
--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { Imagem } from './imagem.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-banner',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+  styleUrl: './banner.component.css',
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      })),
+      transition('escondido <=> visivel', animate('2s ease-in')),
+    ])
+  ]
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
+  public estado: string = 'escondido';
+  public imagens: Imagem[] = [
+    { estado: 'visivel', url: '/banner-acesso/img_1.png' },
+    { estado: 'escondido', url: '/banner-acesso/img_2.png' },
+    { estado: 'escondido', url: '/banner-acesso/img_3.png' },
+    { estado: 'escondido', url: '/banner-acesso/img_4.png' },
+    { estado: 'escondido', url: '/banner-acesso/img_5.png' }
+  ];
+  constructor() { }
 
+  ngOnInit(): void {
+    setTimeout(() => this.logicaRotacao(), 3000);
+  }
+
+  public logicaRotacao(): void {
+    //auxiliar na exibição da imagem seguinte
+    let idx: number = 0;
+    
+    //ocultar imagem
+    for(let i: number = 0; i <= this.imagens.length - 1; i++) {
+      
+      if(this.imagens[i].estado === 'visivel') {
+        this.imagens[i].estado = 'escondido';
+        
+        idx = i === this.imagens.length - 1 ? 0 : i + 1;
+        break;
+      }
+    }
+
+    this.imagens[idx].estado = 'visivel';
+    setTimeout(() => this.logicaRotacao(), 3000);
+  }
 }
