194d23d31c794438bc3163abcb1595ad9cc2e0b5
Carlos Abraão <carlosabraao@hotmail.com.br>
2024-08-07 11:05:39 -0300

    removing console.logs from code

--- src/app/services/autenticacao.service.ts
+++ src/app/services/autenticacao.service.ts
@@ -3,8 +3,6 @@ import * as firebase from 'firebase';
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
-        console.log('Chegamos até o serviço: ', usuario);
-
         firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
             .then((resposta: any) => {
                 if (usuario.senha) {
@@ -20,9 +18,6 @@ export class Autenticacao {
     }
 
     public autenticar(email: string, senha: string): void {
-        console.log('Email: ', email);
-        console.log('Senha: ', senha);
-        
         firebase.auth().signInWithEmailAndPassword(email, senha)
             .then((resposta: any) => {console.log(resposta)})
             .catch((error: any) => {console.log(error)});
